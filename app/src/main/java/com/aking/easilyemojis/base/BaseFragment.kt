924091a162dcf0a0154158e99a014150c84d35f3
rick <rick@txzing.com>
2023-01-04 10:06:23 +0800

    提交测试

--- app/src/main/java/com/aking/easilyemojis/base/BaseFragment.kt
+++ app/src/main/java/com/aking/easilyemojis/base/BaseFragment.kt
@@ -13,9 +13,8 @@ abstract class BaseFragment(@LayoutRes val layoutRes: Int) : Fragment(layoutRes)
 
     override fun showLoading() {
         if (progressDialog == null) progressDialog = ProgressDialog(requireActivity())
+
         progressDialog?.show( )
-
-
     }
 
     override fun dismissLoading() {
